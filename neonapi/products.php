0eb5cf60d8df00cc4353644c2758b17fbbbf0109
spetruzza <steve.petruzza@gmail.com>
2019-07-23 12:27:03 -0600

    handle products code with print

--- neonapi/products.php
+++ neonapi/products.php
@@ -1,8 +1,20 @@
 <?php 
 
-  $request = $_SERVER['REQUEST_URI'];
+  $url = $_SERVER['REQUEST_URI'];
 
-  echo "Request: ", $request, " ";
+  //echo "Request: ", $url, " ";
+
+  $link_array = explode('/',$url);
+  $productCode = end($link_array);
+
+  if($productCode != "")
+  {
+    echo "Selected product code: ".$productCode;
+  }
+  else
+  {
+    echo "requested all products";
+  }
   
   class MyDB extends SQLite3 {
       function __construct() {
@@ -15,22 +27,58 @@
       echo $db->lastErrorMsg();
    }
 
-   $sql =<<<EOF
-      SELECT * FROM datasets;
-EOF;
+   $sql = $db->prepare("SELECT site FROM datasets WHERE productCode = :productCode");
+   $sql->bindValue(':productCode', $productCode, SQLITE3_TEXT);
 
-   $ret = $db->query($sql);
-   
-   while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
-     $dataset = $row['dataset'];
-     echo $dataset, " ";
-   }
+   $ret = $sql->execute();
 
-   $ret = $db->exec($sql);
    if(!$ret) {
       echo $db->lastErrorMsg();
    } 
+
+   $siteCodes=array();
+
+   while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
+     
+     $site = $row['site'];
+
+     $sqlsite = $db->prepare("SELECT * FROM datasets WHERE productCode = :productCode AND site = :site");
+     $sqlsite->bindValue(':productCode', $productCode, SQLITE3_TEXT);
+     $sqlsite->bindValue(':site', $site, SQLITE3_TEXT);
+
+     $retsite = $sqlsite->execute();
+
+     if(!$retsite) {
+        echo $db->lastErrorMsg();
+     } 
+
+     $availMonths=array();
+     $availUrls=array();
+
+     while($rowsite = $retsite->fetchArray(SQLITE3_ASSOC) ) {
+       $dataset = $rowsite['dataset'];
+       $server = $rowsite['server'];
+       $year = $rowsite['year'];
+       $month = $rowsite['month'];
+       $pc = $rowsite['productCode'];
+       
+       array_push($availMonths, $year."_".$month);
+       array_push($availUrls, "&server=".urlencode($server)."&dataset=".urlencode($dataset));
+     }
+
+     array_push($siteCodes, array('siteCode' => $site, 'availableMonths' => array_values($availMonths), 'availableDataUrls' => array_values($availUrls)));
+     
+   }
+
    $db->close();
+
+   $reply = array('data' => array('productCode' => $productCode, 'siteCodes' => array_values($siteCodes)));
+
+   echo "<pre>";
+   echo json_encode($reply, JSON_PRETTY_PRINT);
+   echo "</pre>";
    
+   //{"data":
+   //{"productCode":"DP3.30010.001","0":"siteCodes","1":[{"siteCode":"ORNL","availableMonths":["2016_2"],"availableDataUrls":["&server=https%3A%2F%2Fdataportal.sci.utah.edu&dataset=P3.30010.001-D07-2016_ORNL_2-L3-Camera-Mosaic-V1"]},{"siteCode":"TALL","availableMonths":["2019_5","2018_4","2017_3"],
 ?>
 
